3431091e6b5075d2826268b49000572c30c2bbdd
KobOlek <kobolek25092008@gmail.com>
2026-02-23 15:40:37 +0200

    feat: add admin data to database

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 TOKEN = "BOT_TOKEN"
-admin_ids = [123456789]
+admin_ids = [123456789, 123456789]
+
+admins = {
+    "admin1": admin_ids[0],
+    "admin2": admin_ids[1],
+}
 
 DB_NAME = "DB_NAME"
--- database.py
+++ database.py
@@ -113,7 +113,11 @@ class Database:
             self.insert_defense_dates(subject, subgroup, defense_date)
 
         for name, user_id in admins.items():
-            self.execute(f"INSERT INTO Users (user_id, full_name) VALUES (?, ?)", (user_id, name,))
+            self.execute("INSERT OR IGNORE INTO Users (user_id, full_name) VALUES (?, ?)", (user_id, name,))
+
+        settings_count = self.fetch("SELECT COUNT(*) FROM Settings")
+        if settings_count and settings_count[0][0] == 0:
+            self.execute("INSERT INTO Settings (registration_enabled) VALUES (1)")
 
     def insert_defense_dates(self, subject: str, subgroup: str, defense_date: str):
         parsed_date = datetime.strptime(defense_date, "%d.%m.%y")
